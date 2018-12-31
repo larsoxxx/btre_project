b6e3c1fedea261fa5b3e4934d85fdb47a315ca25
larsoxxx <larry.ochsner@gmail.com>
2018-12-30 17:46:06 -0800

    Removed email

--- btre/settings.py
+++ btre/settings.py
@@ -144,12 +144,13 @@ MESSAGE_TAGS = {
 
 # Email config
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+'''
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER='larry.ochsner@gmail.com'
 EMAIL_HOST_PASSWORD='jzgjvtkygmewrgzs'
 EMAIL_USE_TLS=True
-
+'''
 
 '''
 EMAIL_BACKEND = 'bandit.backends.smtp.HijackSMTPBackend'
